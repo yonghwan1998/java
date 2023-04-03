975206631c713cccf7c5b0aa53f4f6ce6d20f312
Bang <Bang@192.168.1.104>
2023-04-03 16:11:08 +0900

    break 학습

--- java_1/src/basic/BreakApp.java
+++ java_1/src/basic/BreakApp.java
@@ -4,33 +4,35 @@ package basic;
 
 public class BreakApp {
 	public static void main(String[] args) {
-		
-		//반복문은 조건식의 결과가 거짓인 경우 반복문 종료
-		
-		for(int i=1;i<=5;i++) {
-			if(i==3) break;//if 구문의 종료이 참인 경우 break 구문을 이용하여 반복문 종료
-			System.out.print(i+"\t");
+
+		// 반복문은 조건식의 결과가 거짓인 경우 반복문 종료
+
+		for (int i = 1; i <= 5; i++) {
+			if (i == 3)
+				break;// if 구문의 종료이 참인 경우 break 구문을 이용하여 반복문 종료
+			System.out.print(i + "\t");
 		}
 		System.out.println();
 		System.out.println("==============================================================");
-		
-		for(int i=1;i<=5;i++) {
-			for(int j=1;j<=5;j++) {
-				if(j==3) break;//for(int j=1;j<=5;j++) 반복문 종료
-				System.out.println("i = "+i+", j = "+j);
+
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 5; j++) {
+				if (j == 3)
+					break;// for(int j=1;j<=5;j++) 반복문 종료
+				System.out.println("i = " + i + ", j = " + j);
 			}
 		}
 		System.out.println("==============================================================");
-		
-		//반복문 작성시 반복문을 구분하기 위한 식별자(라벨명) 선언 가능
-		//형식) 라벨명:반복문
-		
-		loop:
-		for(int i=1;i<=5;i++) {
-			for(int j=1;j<=5;j++) {
-				//break 라벨명 : 라벨명으로 지정된 반복문 종료
-				if(j==3) break loop;//for(int i=1;i<=5;i++) 반복문 종료
-				System.out.println("i = "+i+", j = "+j);
+
+		// 반복문 작성시 반복문을 구분하기 위한 식별자(라벨명) 선언 가능
+		// 형식) 라벨명:반복문
+
+		loop: for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 5; j++) {
+				// break 라벨명 : 라벨명으로 지정된 반복문 종료
+				if (j == 3)
+					break loop;// for(int i=1;i<=5;i++) 반복문 종료
+				System.out.println("i = " + i + ", j = " + j);
 			}
 		}
 		System.out.println("==============================================================");
