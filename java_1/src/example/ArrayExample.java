b65ddd9f211fbbb378d98c9892f208094cda76fe
Bang <Bang@DESKTOP-QFH9UC6>
2023-04-04 18:24:58 +0900

    array 예제

---
+++ java_1/src/example/ArrayExample.java
@@ -0,0 +1,45 @@
+package example;
+
+public class ArrayExample {
+	public static void main(String[] args) {
+		//사람들의 나이를 저장한 배열 생성하여 참조변수에 저장		
+		int[] age={27,16,22,36,57,60,43,23,14,29,44,52,59,51,39,33,11};
+		
+		//배열에 저장된 모든 사람들의 나이 평균을 계산하여 출력하세요.
+		
+		int sum = 0;
+		int average = 0;
+		
+		for (int i = 0; i < age.length; i++) {
+			sum +=age[i];
+		}
+		average = sum/age.length;
+		
+		System.out.println("모든 사람들의 나이 평균은 "+average+"세이다.");
+		
+		System.out.println("===============================================================");
+		//배열에 저장된 사람들의 나이를 연령별로 구분하여 인원수를 계산하여 출력하세요.
+		//ex) 10대 = 3명
+		//    20대 = 4명
+		//    ...
+		//    60대 = 1명
+	
+		// count[0] - 60, count[1] - 50, count[2] - 40, count[3] - 30, count[4] - 20, count[5] - 10
+		
+		int primeAge = 0;
+		for (int i = 0; i < age.length; i++) {
+			if(primeAge < age[i]) {
+				primeAge = age[i];
+			}
+		}
+		
+		int countArray = primeAge / 10;
+		
+		int[] count = new int[countArray];
+		for (int i = 0; i < age.length; i++) {
+		}
+		
+		System.out.println("===============================================================");
+	}
+	
+}
