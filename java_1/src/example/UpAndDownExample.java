9de80f7d59c17f766dafb4d6e122a0e3d3c91231
Bang <Bang@DESKTOP-QFH9UC6>
2023-04-05 11:27:36 +0900

    가독성 증가

--- java_1/src/example/UpAndDownExample.java
+++ java_1/src/example/UpAndDownExample.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 // => 난수값과 입력값이 같은 경우 입력 횟수 출력 후 프로그램 종료
 // => 난수값과 입력값이 다른 경우 "큰값 입력" 또는 "작은값 입력" 형식의 메세지 출력
 // => 난수값을 10번 안에 맞추지 못한 경우 난수값이 출력되도록 작성
+
 public class UpAndDownExample {
 	public static void main(String[] args) {
 
@@ -18,29 +19,28 @@ public class UpAndDownExample {
 		int failCount = 0;
 
 		start: while (true) {
-			System.out.println("(1~100) 범위의 정수를 입력하세요 >>");
+			System.out.print("(1~100) 범위의 정수를 입력하세요. \n> ");
 			inputNum = scanner.nextInt();
 			if (inputNum < 1 || inputNum > 100) {
 				continue start;
 			}
 			if (failCount == 9) {
-				System.out.println("실패! [정답은 " + comNum + "입니다.]");
+				System.out.println("\n실패! [정답은 " + comNum + "입니다.]");
 				break;
-			}
-			else if (inputNum == comNum) {
-				System.out.println("성공! [실패 횟수는 " + failCount + "입니다.]");
+			} else if (inputNum == comNum) {
+				System.out.println("\n성공! [정답은 " + comNum + "입니다.]\n" + "[실패 횟수는 " + failCount + "회 입니다.]");
 				break;
 			} else if (inputNum > comNum) {
 				failCount++;
 				System.out.println();
-				System.out.println("현재 실패 횟수는 " + failCount + "입니다.(남은 도전 횟수 : " + (10 - failCount)+")");
-				System.out.println("입력된 값 : "+inputNum);
+				System.out.println("현재 실패 횟수는 " + failCount + "회 입니다.(남은 도전 횟수 : " + (10 - failCount) + ")");
+				System.out.println("입력된 값 : " + inputNum);
 				System.out.println("더 작은 값을 입력하세요.");
 			} else if (inputNum < comNum) {
 				failCount++;
 				System.out.println();
-				System.out.println("현재 실패 횟수는 " + failCount + "입니다.(남은 도전 횟수 : " + (10 - failCount)+")");
-				System.out.println("입력된 값 : "+inputNum);
+				System.out.println("현재 실패 횟수는 " + failCount + "입니다.(남은 도전 횟수 : " + (10 - failCount) + ")");
+				System.out.println("입력된 값 : " + inputNum);
 				System.out.println("더 큰 값을 입력하세요.");
 			}
 		}
