dc50112db52e9e3caaaf75d5220b202707b2d9a6
Bang <Bang@DESKTOP-QFH9UC6>
2023-04-06 16:23:49 +0900

    class 학습

--- java_2/src/oop/Car.java
+++ java_2/src/oop/Car.java
@@ -14,43 +14,117 @@ package oop;
 //          ...
 //      }
 
+// 클래스를 작성할 때 필드와 메소드에 접근 제한자를 사용하여 접근 허용 설정 가능
+// 접근 제한자(Access Modifier) : private, package(default), protected, public
+// => 클래스, 필드, 생성자, 메소드를 선언할 때 접근 허용을 설정하기 위한 키워드
+// private : 클래스 내부에서만 접근 가능하도록 허용하는 접근 제한자
+// => 필드, 생성자, 메소드를 은닉화 처리하기 위해 사용 - 클래스 외부에서 접근할 경우 에러 발생
+// => 일반적으로 필드에 사용하는 접근 제한자 - 객체로 필드에 직접적인 접근을 제한하기 위한 사용
+// => 데이타 은닉화(Data Hiding) : 값을 숨겨 보호하기 위한 기능
+// public : 모든 패키지의 클래스에서 접근 가능하도록 허용하는 접근 제한자
+// => 일반적으로 메소드에 사용하는 접근 제한자 - 프로그램 작성에 필요한 모든 클래스에서 접근 할 수 있도록 허용
+
 // 자동차를 객체 모델하여 클래스로 작성
 // => 속성 : 모델명, 엔진상태, 현재속도 - 필드
 // => 행위 : 시동 온(On), 시동 오프(Off), 속도 증가, 속도 감소, 이동 중지 - 메소드
 
 public class Car {
-	
 	// 필드(Field) : 클래스 내부에 선언된 모든 메소드는 필드 접근 가능
-	String modelName;// 모델명
-	boolean engineStatus;// 엔진상태 - false : EngineOff, true : EngineOn
-	int currentSpeed;// 현재속도
+	private String modelName;// 모델명
+	private boolean engineStatus;// 엔진상태 - false : EngineOff, true : EngineOn
+	private int currentSpeed;// 현재속도
 
 	// 생성자(Constructor) : 생성자를 선언하지 않으면 매개변수가 없는 기본 생성자(Default
 	// Constructor)가 자동으로 생성되어 제공
 
 	// 메소드(Method) : 필드를 사용하여 메소드의 명령으로 필요한 기능을 제공되도록 작성
-	void startEngine() {// 시동 온(On)
+	public void startEngine() {// 시동 온(On)
 		engineStatus = true;
 		System.out.println(modelName + "의 자동차 시동을 켰습니다.");
 	}
 
-	void stopEngine() {// 시동 오프(Off)
+	public void stopEngine() {// 시동 오프(Off)
+		if (currentSpeed != 0) {// 자동차가 멈췄있지 않은 상태인 경우
+
+			// 클래스 내부에 선언된 메소드는 서로 호출 가능
+			// => 코드의 중복성을 최소화 하여 프로그램의 생산성 및 유지보수의 효율성 증가
+			speedZero();
+		}
+
 		engineStatus = false;
 		System.out.println(modelName + "의 자동차 시동을 껐습니다.");
 	}
 
-	void speedUp(int speed) {// 속도 증가
+	public void speedUp(int speed) {// 속도 증가
+		if (!engineStatus) {// 엔진이 꺼져 있는 경우
+			System.out.println(modelName + "의 자동차 시동이 꺼져 있습니다.");
+			return;// 메소드 종료
+		}
+
+		if (currentSpeed + speed > 150) {
+			speed = 150 - currentSpeed;
+		}
+
 		currentSpeed += speed;
 		System.out.println(modelName + "의 자동차 속도가 " + speed + "Km/h 증가 되었습니다. 현재 속도는 " + currentSpeed + "Km/h 입니다.");
 	}
 
-	void speedDown(int speed) {// 속도 감소
+	public void speedDown(int speed) {// 속도 감소
+		if (!engineStatus) {// 엔진이 꺼져 있는 경우
+			System.out.println(modelName + "의 자동차 시동이 꺼져 있습니다.");
+			return;// 메소드 종료
+		}
+
+		if (currentSpeed < speed) {
+			speed = currentSpeed;
+		}
+
 		currentSpeed -= speed;
 		System.out.println(modelName + "의 자동차 속도가 " + speed + "Km/h 감소 되었습니다. 현재 속도는 " + currentSpeed + "Km/h 입니다.");
 	}
 
-	void speedZero() {// 이동 중지
+	public void speedZero() {// 이동 중지
 		currentSpeed = 0;
 		System.out.println(modelName + "의 자동차가 멈췄습니다.");
+	}
+
+	// 은닉화 처리된 필드를 위해 필드값을 반환하는 Getter 메소드와 필드값 변경하는
+	// Setter 메소드 선언 - 캡슐화
+	// 캡슐화(Encapsulation) : 표현대상의 속성(필드)과 행위(메소드)를 하나의 자료형(클래스)로 작성
+	// => 필드를 은닉화 처리하여 보호하고 메소드를 이용하여 처리되도록 설정
+
+	// Getter 메소드 : 클래스에 외부에서 필드값을 사용할 수 있도록 필드값을 반환하는 메소드
+	// => 필드의 자료형이 [boolean]인 경우에는 메소드의 이름을 [is필드명]으로 작성
+	// 형식) public 반환형 get필드명() { return 필드명; }
+	
+	public String getModelName() {
+		return modelName;
+	}
+
+	// Setter 메소드 : 매개변수로 값을 전달받아 매개변수에 저장된 값으로 필드값을 변경하는 메소드
+	// 형식) public void set필드명(자료형 변수명) { 필드명=변수명; }
+	// => 매개변수에 전달되어 저장된 값에 대한 검증 가능
+	public void setModelName(String modelName) {
+		// this : 메소드 내부에서 클래스의 객체를 표현하는 키워드
+		// => this 키워드를 사용하여 필드 표현
+		this.modelName = modelName;
+	}
+
+	public boolean isEngineStatus() {
+		return engineStatus;
+	}
+
+	public void setEngineStatus(boolean engineStatus) {
+		this.engineStatus = engineStatus;
+	}
+
+	public int getCurrentSpeed() {
+		return currentSpeed;
+	}
+
+	public void setCurrentSpeed(int currentSpeed) {
+		if (currentSpeed > 150)
+			return;
+		this.currentSpeed = currentSpeed;
 	}
 }
--- java_2/src/oop/CarApp.java
+++ java_2/src/oop/CarApp.java
@@ -1,21 +1,18 @@
 package oop;
 
 // 클래스로 객체를 생성하는 방법
-//형식) 클래스명 참조변수 = new 클래스명();
-// => new 연산자로 클래스의 기본 생성자를 호출하여 객체를 생성하고 생성된 객체의 메모리 
-// 주소를 참조변수에 저장
+// 형식) 클래스명 참조변수 = new 클래스명();
+// => new 연산자로 클래스의 기본 생성자를 호출하여 객체를 생성하고 생성된 객체의 메모리 주소를 참조변수에 저장
 // => 참조변수에 저장된 객체는 . 연산자를 사용하여 필드 또는 메소드에 이용해 프로그램 작성  
 
 //Car 클래스를 객체로 생성하여 작성된 프로그램
 
 public class CarApp {
 	public static void main(String[] args) {
-		
 		// Car 클래스를 객체를 생성하여 참조변수에 저장
 		// => 하나의 클래스로 서로 다른 객체를 여러개 생성 가능
 		// => 클래스는 객체를 생성하기 위한 틀(Template)
-		// => 객체를 생성하면 객체의 필드에서는 기본값(숫자형 : 0, 논리형 : false,
-		// 참조형 : null)이 초기값으로 자동 저장
+		// => 객체를 생성하면 객체의 필드에서는 기본값(숫자형 : 0, 논리형 : false, 참조형 : null)이 초기값으로 자동 저장
 		Car carOne = new Car();
 		Car carTwo = new Car();
 		Car carThree = new Car();
@@ -27,20 +24,23 @@ public class CarApp {
 		
 		// 참조변수.필드명 : 참조변수에 저장된 객체가 . 연산자를 사용하여 필드에 접근
 		// => 객체의 필드에는 기본값이 초기값으로 자동 저장
-		carOne.modelName = "싼타페";// 객체의 필드값 변경
+		// 문제점)객체를 사용하여 필드에 직접적인 접근을 허용하면 필드에 비정상적인 값 저장 가능
+		// 해결법)클래스 작성시 필드를 은닉화 처리하여 선언 - 은닉화 선언된 필드에 접근할 경우 에러 발생
+		carOne.setModelName("싼타페");// 필드값을 변경하는 Setter 메소드 호출
 
-		System.out.println("첫번째 자동차 모델명 = " + carOne.modelName);// 객체의 필드값 사용
-		System.out.println("첫번째 자동차 엔진상태 = " + carOne.engineStatus);
-		System.out.println("첫번째 자동차 현재속도 = " + carOne.currentSpeed);
+		// 객체의 필드값을 반환하는 Getter 메소드 호출 - 반환받은 필드값 사용(출력)
+		System.out.println("첫번째 자동차 모델명 = " + carOne.getModelName());
+		System.out.println("첫번째 자동차 엔진상태 = " + carOne.isEngineStatus());
+		System.out.println("첫번째 자동차 현재속도 = " + carOne.getCurrentSpeed());
 		System.out.println("==============================================================");
-		
-		carTwo.modelName = "쏘나타";
-		carTwo.engineStatus = true;
-		carTwo.currentSpeed = 80;
 
-		System.out.println("두번째 자동차 모델명 = " + carTwo.modelName);
-		System.out.println("두번째 자동차 엔진상태 = " + carTwo.engineStatus);
-		System.out.println("두번째 자동차 현재속도 = " + carTwo.currentSpeed);
+		carTwo.setModelName("쏘나타");
+		carTwo.setEngineStatus(true);
+		carTwo.setCurrentSpeed(80);
+
+		System.out.println("두번째 자동차 모델명 = " + carTwo.getModelName());
+		System.out.println("두번째 자동차 엔진상태 = " + carTwo.isEngineStatus());
+		System.out.println("두번째 자동차 현재속도 = " + carTwo.getCurrentSpeed());
 		System.out.println("==============================================================");
 		
 		// 참조변수.메소드명(값,값...) : 참조변수에 저장된 객체가 . 연산자를 사용하여 메소드 호출
@@ -48,9 +48,7 @@ public class CarApp {
 		carOne.speedUp(50);
 		carOne.speedUp(30);
 		carOne.speedDown(40);
-		carOne.speedZero();
 		carOne.stopEngine();
 		System.out.println("==============================================================");
-		
 	}
 }
