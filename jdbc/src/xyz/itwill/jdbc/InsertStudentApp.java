b3329b49b41fd29625581f3e2f64b380f83a224a
Bang <Bang@192.168.1.73>
2023-05-12 15:01:41 +0900

    jdbc insert student 학습

--- jdbc/src/xyz/itwill/jdbc/InsertStudentApp.java
+++ jdbc/src/xyz/itwill/jdbc/InsertStudentApp.java
@@ -60,8 +60,7 @@ public class InsertStudentApp {
 			// => DBMS 서버에 접속된 정보가 저장된 Connection 객체를 반환
 			// => 접속 URL 주소의 프로토콜을 이용하여 특정 DBMS 서버에 접속
 			// URL(Uniform Resource Location) : 인터넷에 존재하는 자원의 위치를 표현하는 주소
-			// 형식)Protocol:ServerName:Port:Resource >>
-			// http://www.itwill.xyz:80/test/index.html
+			// 형식)Protocol:ServerName:Port:Resource >> http://www.itwill.xyz:80/test/index.html
 			// Oracle DBMS 서버에 접속하여 데이타베이스에 접근하기 위한 URL 주소
 			// 형식)jdbc:oracle:thin:@ServerName:Port:SID
 			// JDBC 관련 클래스의 메소드를 호출한 경우 반드시 SQLException 발생 - 일반예외
@@ -82,15 +81,13 @@ public class InsertStudentApp {
 			// => DML 명령의 실행결과로 조작행의 갯수가 저장된 정수값를 반환
 			// Statement.executeQuery(String sql) : SELECT 명령을 전달하여 실행하는 메소드
 			// => SELECT 명령의 실행결과로 검색행이 저장된 ResultSet 객체를 반환
-			// String sql="insert into student values(1000,'홍길동','010-1234-5678','서울시
-			// 강남구','00/01/01')";
-			// String sql="insert into student values(2000,'임꺽정','010-7894-5612','수원시
-			// 월정구','02/05/08')";
+			// String sql="insert into student values(1000,'홍길동','010-1234-5678','서울시 강남구','00/01/01')";
+			// String sql="insert into student values(2000,'임꺽정','010-7894-5612','수원시 월정구','02/05/08')";
 			String sql = "insert into student values(3000,'전우치','010-1478-2589','인천시 상당구','1998-12-11')";
 			int rows = stmt.executeUpdate(sql);
 
 			// 5.반환받은 SQL 명령의 실행 결과값을 사용자에게 제공
-			System.out.println("[메세지]" + rows + "명의 학생정보를 삽입 하였습니다.");
+			System.out.println("[메세지]" + rows + "명의 학생정보를 삽입했습니다.");
 		} catch (ClassNotFoundException e) {
 			System.out.println("[에러]OracleDriver 클래스를 찾을 수 없습니다.");
 		} catch (SQLException e) {
