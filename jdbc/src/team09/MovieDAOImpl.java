cad38eaa1d82c36c040232e6795cde76efe56e7f
Bang <Bang@192.168.1.73>
2023-05-19 16:25:36 +0900

    selectAllMovieList() 생성

--- jdbc/src/team09/MovieDAOImpl.java
+++ jdbc/src/team09/MovieDAOImpl.java
@@ -1,5 +1,10 @@
 package team09;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MovieDAOImpl extends JdbcDAO implements MovieDAO {
@@ -19,7 +24,35 @@ public class MovieDAOImpl extends JdbcDAO implements MovieDAO {
 
 	@Override
 	public List<MovieDTO> selectAllMovieList() {
-		// TODO Auto-generated method stub
-		return null;
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<MovieDTO> movieList = new ArrayList<>();
+		try {
+			con = getConnection();
+
+			String sql = "select * from MOVIE_INFO order by no";
+			pstmt = con.prepareStatement(sql);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				MovieDTO movie = new MovieDTO();
+				movie.setMOVIE_NO(rs.getInt("MOVIE_NO"));
+				movie.setMOVIE_TITLE(rs.getString("MOVIE_TITLE"));
+				movie.setMOVIE_GENRE(rs.getString("MOVIE_GENRE"));
+				movie.setMOVIE_TIME(rs.getString("MOVIE_TIME"));
+				movie.setMOVIE_COUNTRY(rs.getString("MOVIE_COUNTRY"));
+				movie.setMOVIE_DIRECTOR(rs.getString("MOVIE_DIRECTOR"));
+
+				movieList.add(movie);
+			}
+		} catch (SQLException e) {
+			System.out.println("[에러]selectAllStudentList() 메소드의 SQL 오류 = " + e.getMessage());
+		} finally {
+			close(con, pstmt, rs);
+		}
+		return movieList;
 	}
 }
