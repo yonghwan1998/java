06feb2a4505a26e21c1f1fba897e6af1a021cc3c
yonghwan1998 <bbl737898@gmail.com>
2023-07-11 12:07:58 +0900

    MVC model2

--- mvc/src/main/java/xyz/itwill/mvc/ViewModel.java
+++ mvc/src/main/java/xyz/itwill/mvc/ViewModel.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import xyz.itwill.dto.UserinfoDTO;
+import xyz.itwill.service.UserinfoService;
 
 //클라이언트가 [/view.do]로 요청한 경우 객체로 생성될 모델 역활의 클래스
 // => 로그인 상태의 사용자만 요청 가능하도록 권한 설정
@@ -15,8 +19,35 @@ public class ViewModel implements Action {
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		return null;
+		ActionForward actionForward = new ActionForward();
+		try {
+			HttpSession session = request.getSession();
+			UserinfoDTO loginUserinfo = (UserinfoDTO) session.getAttribute("loginUserinfo");
+			// 비로그인 상태의 사용자인 경우 - 비정상적인 요청
+			if (loginUserinfo == null) {
+				throw new Exception();
+			}
+
+			if (request.getParameter("userid") == null) {// 전달값이 없는 경우 - 비정상적인 요청
+				throw new Exception();
+			}
+
+			String userid = request.getParameter("userid");
+
+			// UserinfoService 클래스의 getUserinfo() 메소드를 호출하여 검색된 회원정보를
+			// DTO 객체로 반환받아 request 객체의 속성값 저장
+			// => UserinfoService 클래스의 getUserinfo() 메소드를 호출하여 검색된 회원정보가
+			// 없는 경우 UserinfoNotFoundException 발생 - 비정상적인 요청
+			request.setAttribute("userinfo", UserinfoService.getService().getUserinfo(userid));
+
+			actionForward.setForward(true);
+			actionForward.setPath("/model_two/user_view.jsp");
+		} catch (Exception e) {
+			e.printStackTrace();
+			actionForward.setForward(false);
+			actionForward.setPath(request.getContextPath() + "/error.do");
+		}
+		return actionForward;
 	}
 
 }
--- mvc/src/main/webapp/model_two/user_list.jsp
+++ mvc/src/main/webapp/model_two/user_list.jsp
@@ -2,7 +2,7 @@
 <%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%-- request 객체의 속성값으로 저장된 회원정보를 반환받아 클라이언트에게 전달하여 응답하는 JSP 문서 --%>
+<%-- request 객체의 속성값으로 저장된 회원목록을 반환받아 클라이언트에게 전달하여 응답하는 JSP 문서 --%>
 <%-- => 회원정보에서 [회원이름] 태그를 클릭한 경우 [view.do] 페이지 요청 - 아이디 전달 --%>
 <%-- => [회원등록] 태그를 클릭한 경우 [writeform.do] 페이지 요청 - 관리자에게만 링크 제공 --%>
 <%-- => [로그아웃] 태그를 클릭한 경우 [logout.do] 페이지 요청 --%>
