3dcf8f9a4ad07a45fa04462d382536389079e451
yonghwan1998 <bbl737898@gmail.com>
2023-07-17 12:50:36 +0900

    mybatis Student

--- mybatis/src/main/java/xyz/itwill/dao/StudentDAO.java
+++ mybatis/src/main/java/xyz/itwill/dao/StudentDAO.java
@@ -53,16 +53,20 @@ public class StudentDAO {
 	// => SqlSession 객체의 메소드를 호출하여 매퍼에 등록된 정보를 이용하여 SQL 명령을 전달하여
 	// 실행하고 결과를 Java 객체로 반환받아 사용
 	public List<Student> selectStudentList() {
-
+		// SqlSessionFactory.openSession() : SqlSession 객체를 생성하여 반환하는 메소드
+		// => SqlSession 객체 : 매퍼에 등록된 정보를 이용하여 SQL 명령을 제공받고 실행하고
+		// 실행결과를 Java 객체로 매핑 처리하여 반환하는 기능을 제공하는 객체
 		SqlSession sqlSession = getSqlSessionFactory().openSession();
-		
-		
 		try {
+			// SqlSession.selectList(String elementId) : 매퍼에 등록된 SELECT 명령을 제공받아
+			// DBMS 서버에 전달하여 실행하고 실행결과를 List 객체로 반환하는 메소드
+			// => elementId 매개변수에는 매퍼 식별자(mapper 엘리먼트의 namespace 속성값)와
+			// select 엘리먼트의 식별자(id 속성값)을 이용한 문자열을 전달받아 매퍼에 등록된 SQL
+			// 명령을 DBMS 서버에 전달하여 실행하고 실행결과를 Java 객체로 매핑 처리하여 반환
 			return sqlSession.selectList("xyz.itwill.mapper.StudentMapper.selectStudentList");
-		}finally {
+		} finally {
+			// SqlSession.close() : SqlSession 객체가 사용한 JDBC 관련 객체를 제거하는 메소드
 			sqlSession.close();
 		}
-		
-		
 	}
 }
