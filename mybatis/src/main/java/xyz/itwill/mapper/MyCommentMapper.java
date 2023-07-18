3651bfff1c408859294abbf8e4dccb224722c064
yonghwan1998 <bbl737898@gmail.com>
2023-07-18 16:06:39 +0900

    mybatis MyComment1

---
+++ mybatis/src/main/java/xyz/itwill/mapper/MyCommentMapper.java
@@ -0,0 +1,11 @@
+package xyz.itwill.mapper;
+
+import java.util.List;
+
+import xyz.itwill.dto.MyComment1;
+
+public interface MyCommentMapper {
+	int insertComment1(MyComment1 comment);
+
+	List<MyComment1> selectCommentList1();
+}
