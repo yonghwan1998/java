d22b1947bc83d44fb5433b04133e18847d02aee7
yonghwan1998 <bbl737898@gmail.com>
2023-07-18 13:00:21 +0900

    mybatis MyMember

---
+++ mybatis/src/main/java/xyz/itwill/dao/MyUserDAO.java
@@ -0,0 +1,42 @@
+package xyz.itwill.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import xyz.itwill.dto.MyUser;
+import xyz.itwill.mapper.MyUserMapper;
+
+public class MyUserDAO extends AbstractSession {
+	private static MyUserDAO _dao;
+
+	private MyUserDAO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	static {
+		_dao = new MyUserDAO();
+	}
+
+	public static MyUserDAO getDAO() {
+		return _dao;
+	}
+
+	public int insertUser(MyUser user) {
+		SqlSession sqlSession = getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyUserMapper.class).insertUser(user);
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	public List<MyUser> selectUserList() {
+		SqlSession sqlSession = getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyUserMapper.class).selectUserList();
+		} finally {
+			sqlSession.close();
+		}
+	}
+}
