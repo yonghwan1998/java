ffe4751d1c5f2b1feb3b6011994b28e977fdefe3
yonghwan1998 <bbl737898@gmail.com>
2023-07-18 18:01:01 +0900

    mybatis MyComment2

---
+++ mybatis/src/main/java/xyz/itwill/dto/MyComment2.java
@@ -0,0 +1,59 @@
+package xyz.itwill.dto;
+
+//테이블의 컬럼명과 다른 이름으로 클래스의 필드명을 작성한 클래스 - 수동 매핑
+public class MyComment2 {
+	private int no;
+	private String id;
+	private String content;
+	private String date;
+
+	public MyComment2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyComment2(int no, String id) {
+		super();
+		this.no = no;
+		this.id = id;
+	}
+
+	public MyComment2(int no, String id, String content, String date) {
+		super();
+		this.no = no;
+		this.id = id;
+		this.content = content;
+		this.date = date;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+}
--- mybatis/src/main/java/xyz/itwill/mapper/MyCommentMapper.java
+++ mybatis/src/main/java/xyz/itwill/mapper/MyCommentMapper.java
@@ -3,6 +3,7 @@ package xyz.itwill.mapper;
 import java.util.List;
 
 import xyz.itwill.dto.MyComment1;
+import xyz.itwill.dto.MyComment2;
 
 public interface MyCommentMapper {
 	int insertComment1(MyComment1 comment);
@@ -10,4 +11,6 @@ public interface MyCommentMapper {
 	int insertComment2(MyComment1 comment);
 
 	List<MyComment1> selectCommentList1();
+
+	List<MyComment2> selectCommentList2();
 }
