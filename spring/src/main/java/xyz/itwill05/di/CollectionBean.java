425d1e237090d900af53e63bd636d9087cb535b2
yonghwan1998 <bbl737898@gmail.com>
2023-07-27 14:40:15 +0900

    spring DI 학습

---
+++ spring/src/main/java/xyz/itwill05/di/CollectionBean.java
@@ -0,0 +1,50 @@
+package xyz.itwill05.di;
+
+import java.util.List;
+import java.util.Set;
+
+public class CollectionBean {
+	private Set<String> nameSet;
+	private List<String> nameList;
+	// 콜렉션 객체의 제네릭을 인터페이스로 설정하면 콜렉션 객체에는 인터페이스를 상속받은
+	// 모든 자식클래스로 생성된 객체를 요소 추가 가능
+	private Set<Controller> controllerSet;
+	private List<Controller> controllerList;
+
+	public CollectionBean() {
+		System.out.println("### CollectionBean 클래스의 기본 생성자 호출 ###");
+	}
+
+	public Set<String> getNameSet() {
+		return nameSet;
+	}
+
+	public void setNameSet(Set<String> nameSet) {
+		this.nameSet = nameSet;
+	}
+
+	public List<String> getNameList() {
+		return nameList;
+	}
+
+	public void setNameList(List<String> nameList) {
+		this.nameList = nameList;
+	}
+
+	public Set<Controller> getControllerSet() {
+		return controllerSet;
+	}
+
+	public void setControllerSet(Set<Controller> controllerSet) {
+		this.controllerSet = controllerSet;
+	}
+
+	public List<Controller> getControllerList() {
+		return controllerList;
+	}
+
+	public void setControllerList(List<Controller> controllerList) {
+		this.controllerList = controllerList;
+	}
+
+}
---
+++ spring/src/main/java/xyz/itwill05/di/CollectionBeanApp.java
@@ -0,0 +1,22 @@
+package xyz.itwill05.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CollectionBeanApp {
+	public static void main(String[] args) {
+		System.out.println("=============== Spring Container 초기화 전 ===============");
+		ApplicationContext context = new ClassPathXmlApplicationContext("05-2_collection.xml");
+		System.out.println("=============== Spring Container 초기화 후 ===============");
+		CollectionBean bean = context.getBean("collectionBean", CollectionBean.class);
+
+		// CollectionBean 객체의 필드값(Collection 객체)을 반환받아 출력
+		// => Collection 객체의 toString() 메소드 호출 - Collection 객체에 저장된 모든 요소값 출력
+		System.out.println("nameSet = " + bean.getNameSet());
+		System.out.println("nameList = " + bean.getNameList());
+		System.out.println("controllerSet = " + bean.getControllerSet());
+		System.out.println("controllerList = " + bean.getControllerList());
+		System.out.println("==========================================================");
+		((ClassPathXmlApplicationContext) context).close();
+	}
+}
