8e468b20d9b305d8d13ecbd7cae47bbf2f9fa914
yonghwan1998 <bbl737898@gmail.com>
2023-07-27 12:48:30 +0900

    spring DI 학습

--- spring/src/main/java/xyz/itwill05/di/StudentApp.java
+++ spring/src/main/java/xyz/itwill05/di/StudentApp.java
@@ -27,6 +27,21 @@ public class StudentApp {
 		Student student6 = context.getBean("student6", Student.class);
 		System.out.println(student6);
 		System.out.println("==========================================================");
+		// 프로그램 실행에 필요한 데이타 처리 기능을 제공하는 Service 객체를 제공받아 저장
+		// StudentServiceImpl service=context.getBean("studentServiceImpl",
+		// StudentServiceImpl.class);
+
+		// 클래스로 참조변수(필드)를 생성하여 객체를 저장하는 것보다 인터페이스로 참조변수(필드)를
+		// 생성하여 객체를 저장하는 것이 유지보수의 효율성을 증가하는 방법
+		StudentService service = context.getBean("studentServiceImpl", StudentService.class);
+
+		// Service 객체의 메소드를 호출하여 데이타 처리 기능 구현
+		service.addStudent(student1);
+		service.modifyStudent(student1);
+		service.removeStudent(1000);
+		service.getStudent(1000);
+		service.getStudentList();
+		System.out.println("==========================================================");
 		((ClassPathXmlApplicationContext) context).close();
 	}
 }
