291e8f7d93168e2765ef53969098ae1ef47ee1af
yonghwan1998 <bbl737898@gmail.com>
2023-08-01 12:00:19 +0900

    spring AOP 학습

--- spring/src/main/java/xyz/itwill07/aop/EmailSendBean.java
+++ spring/src/main/java/xyz/itwill07/aop/EmailSendBean.java
@@ -1,5 +1,12 @@
 package xyz.itwill07.aop;
 
+//JavaMail 기능을 구현하기 위해 spring-context-support 라이브러리와 javax.mail 라이브러리가
+//프로젝트에 빌드되도록 처리 - 메이븐 이용 : pom.xml
+
+//메일 전송 기능을 제공하기 위한 클래스 - 메일 서버의 SMTP 서비스를 사용하여 메일 전송
+// => 메일 서버(Mail Server) : 메일을 송수신하는 서비스를 제공하는 컴퓨터
+// => SMTP(Simple Message Transfer Protocol) 서비스로 메일을 보내고 POP3(Post Office Protocol 3)
+//서비스나 IMAP(Internet Message Access Protocol) 서비스로 메일을 받아 사용자에 전달
 public class EmailSendBean {
 
 }
