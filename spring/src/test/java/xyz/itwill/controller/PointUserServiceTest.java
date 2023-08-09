055a23e1c03a18ef2c69c9187ba5f649fdb01dea
yonghwan1998 <bbl737898@gmail.com>
2023-08-09 14:28:24 +0900

    spring TransactionManager 학습

---
+++ spring/src/test/java/xyz/itwill/controller/PointUserServiceTest.java
@@ -0,0 +1,30 @@
+package xyz.itwill.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import lombok.extern.slf4j.Slf4j;
+import xyz.itwill10.dto.PointUser;
+import xyz.itwill10.service.PointUserService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/*.xml" })
+@Slf4j
+public class PointUserServiceTest {
+	@Autowired
+	private PointUserService pointUserService;
+
+	@Test
+	public void testAddPointUser() throws Exception {
+		PointUser user = PointUser.builder().id("abc123").name("홍길동").build();
+
+		PointUser addUser = pointUserService.addPointUser(user);
+
+		log.info(addUser.toString());
+	}
+}
